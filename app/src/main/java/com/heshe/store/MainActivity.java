55924fc3c3f93fdc038760f8996659734ada6a3d
RohithV <rohithvegesna@gmail.com>
2016-07-31 08:26:11 +0530

    He-She Store App Build-1
    Introducing New app for He-She Store E-commerce

---
+++ app/src/main/java/com/heshe/store/MainActivity.java
@@ -0,0 +1,125 @@
+package com.heshe.store;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle drawerToggle;
+    NavigationView navigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initInstances();
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setIcon(R.drawable.ic_action_hestore);
+    }
+
+    private void initInstances() {
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.hello_world, R.string.hello_world);
+        drawerLayout.setDrawerListener(drawerToggle);
+        final Activity activityMain = this;
+
+        navigation = (NavigationView) findViewById(R.id.navigation_view);
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                WebView webview = (WebView) activityMain.findViewById(R.id.webView);
+
+                if (id == R.id.navigation_item_1) {
+                    webview.loadUrl("http://www.citeinfo.net/1.php");
+                } else if (id == R.id.navigation_item_2) {
+                    webview.loadUrl("http://www.citeinfo.net/2.php");
+
+                } else if (id == R.id.navigation_item_3) {
+                    webview.loadUrl("http://www.citeinfo.net/3.php");
+
+                } else if (id == R.id.navigation_item_4) {
+                    webview.loadUrl("http://www.citeinfo.net/4.php");
+
+                } else if (id == R.id.navigation_item_5) {
+                    webview.loadUrl("http://www.citeinfo.net/5.php");
+
+                }
+                DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+                drawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+
+        WebView myWebView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+
+        myWebView.loadUrl("http://www.citeinfo.net/index.php");
+    }
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_info) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
